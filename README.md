ce64ad4549ef682b9f161750dfbcf9a12b4068e0
Muhammad Hannan <hannansultan@outlook.com>
2017-12-21 20:27:58 +0500

    solved sudoku

--- README.md
+++ README.md
@@ -1,20 +1,4 @@
 # Artificial Intelligence Nanodegree
-## Introductory Project: Diagonal Sudoku Solver
-
-# Question 1 (Naked Twins)
-Q: How do we use constraint propagation to solve the naked twins problem?  
-A: To solve naked twins problem I had to look for box with only two digits and if a box found with two digits next step
-is to check for it's peers if another box in it's peers containing that values. Remove the similar values form the peers
-and repeat this process untill no twins found.
-
-# Question 2 (Diagonal Sudoku)
-Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: First step is to get the diagonals. For that I need to get all elements at row[1],col[1] then continue
-row and col values plus one. This will get me first diagonal from Top left to right bottom and for second 
-diagonal I need to start from row[1],col[9] and then decrease the column value till 1 and increase the row
-value till 9. I did this using zip method in one go. Then added the diagonals to unitlist this limit the 
-possibilities to find the final solution. As we only need unique element at diagonals we can easily get 
-that find iterating the grid with unitlist values.
 
 ### Install
 
@@ -39,13 +23,4 @@ If not, please see how to download pygame [here](http://www.pygame.org/download.
 ### Visualizing
 
 To visualize your solution, please only assign values to the values_dict using the `assign_value` function provided in solution.py
-
-### Submission
-Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
-
-The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
-
-To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
-
-This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
 
--- solution.py
+++ solution.py
@@ -117,34 +117,6 @@ def display(values):
     print()
 
 
-def eliminate(values):
-    """
-    go through all the boxes,for every solved box,eliminate its value from the value of the
-    all of its peers.
-"""
-    solved_boxes = [box for box in values.keys() if len(values[box]) == 1]
-    for box in solved_boxes:
-        digits = values[box]
-        for peer in peers[box]:
-            assign_value(values, peer, values[peer].replace(digits, ''))
-    return values
-
-
-def only_choice(values):
-    """
-    every unit in unitlist, assign a value for a box if there is a unit with a
-    value that only fits in that particular box.
-    :param values:
-    :return:
-    """
-    for unit in unitlist:
-        for digit in '123456789':
-            digit_exist = [box for box in unit if digit in values[box]]
-            if len(digit_exist) == 1:
-                assign_value(values, digit_exist[0], digit)
-    return values
-
-
 def reduce_puzzle(values):
     """
     use every fuction (eliminate,only choice and naked_twins) in the given function
@@ -152,18 +124,32 @@ def reduce_puzzle(values):
     :param values:
     :return:
     """
-    solved_values = [box for box in values.keys() if len(values[box]) == 1]
     stalled = False
     while not stalled:
+        # Check how many boxes have a determined value
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
-        values = eliminate(values)
-        values = only_choice(values)
-        values = naked_twins(values)
+        solved_values = [box for box in values.keys() if len(values[box]) == 1]
+        # Using the Eliminate Strategy
+        for box in solved_values:
+            digit = values[box]
+            for peer in peers[box]:
+                values[peer] = values[peer].replace(digit, '')
+        # Using the Only Choice Strategy
+        for unit in unitlist:
+            for digit in '123456789':
+                dplaces = [box for box in unit if digit in values[box]]
+                if len(dplaces) == 1:
+                    values[dplaces[0]] = digit
+        # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
         stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
-        return values
+    return values
+
+
 
 
 def search(values):
@@ -171,21 +157,22 @@ def search(values):
     :param values:
     :return:
     """
+
     values = reduce_puzzle(values)
-
-    if values == False:
-        return False
+    if values is False:
+        return False  ## Failed earlier
     if all(len(values[s]) == 1 for s in boxes):
-        return values
-
+        return values  ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
     n, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
-    # here we use recurrence to solve each one of the resulting sudoku.
+    # Now use recurrence to solve each one of the resulting sudokus, and
     for value in values[s]:
         new_sudoku = values.copy()
         new_sudoku[s] = value
         attempt = search(new_sudoku)
         if attempt:
             return attempt
+
 
 
 def solve(grid):
